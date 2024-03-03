4326a6706c9716416751d04e6c7ab5f76187ec9f
Daniel <155811921+dnlaglr@users.noreply.github.com>
2024-03-03 10:20:30 -0800

    Create portfolio sections

---
+++ src/views/intro.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Intro() {
+  return (
+    <div id='intro' className='flex w-screen h-[calc(100vh_-_56.3px)] mt-[56.3px] bg-dark'>
+    </div>
+  )
+}
+
+export default Intro
