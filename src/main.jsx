153b2b1ab41a247dd3d90b025ab0074cbc62528c
Daniel <155811921+dnlaglr@users.noreply.github.com>
2024-03-03 10:20:06 -0800

    Add sections

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,16 @@ import './index.css'
 
 import Navbar from './components/navbar'
 import Intro from './views/intro'
+import About from './views/about'
+import Experience from './views/experience'
+import Projects from './views/projects'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
     <Intro />
+    <About />
+    <Experience />
+    <Projects />
   </React.StrictMode>,
 )
