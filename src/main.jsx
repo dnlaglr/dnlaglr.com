de52fffd0b33c92240f5467e63bfd75a2a0db867
Daniel <155811921+dnlaglr@users.noreply.github.com>
2024-03-03 08:10:25 -0800

    Create navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+import Navbar from './components/navbar'
+import Intro from './views/intro'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Navbar />
+    <Intro />
   </React.StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark": "#181620",
+        "kindadark": "#343a40",
+        "accent": "#CCD6F6"
+      }
+    },
   },
   plugins: [],
 }
